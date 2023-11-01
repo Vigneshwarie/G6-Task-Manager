46d2d684dd723a9761dfc2c56cf96011ec6968ca
Vigneswari Sambandam <vigneswari@Vigneswaris-MacBook-Air.local>
2023-11-01 01:06:14 -0400

    Added function to get data from Local Storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,18 +11,38 @@ var submitBtn = document.querySelector("#submitBtn");
 
 
 // Variable Declarations
-
-
 var isEdit = false;
+var vId;
 
 
 // A function to load the tasks on page load
+//https://stackoverflow.com/questions/63636214/jquery-to-retrieve-multiple-keys-with-same-starting-pattern-from-localstorage
+function getTaskFromLocalStorage() {
+     vId = "TAM-" + dayjs().format('YYYYMMDD');
+     Object.entries(localStorage).forEach(([key, value]) => {
+     if (key.startsWith(vId)) {
+          console.log(key + "====" + value);
+          var divEl = document.createElement("div");
+          divEl.classList.add("task-item");
+          var idAttr = document.createAttribute("item-id");
+          idAttr.value = key;
+          divEl.setAttributeNode(idAttr);
+          divEl.innerHTML = ` <p>${value}</p>
+                         <div>
+                              <button type="button" class="editbtn">Edit</button>
+                              <button type="button" class="deletebtn">Delete</button>
+                         </div>`;
+          taskList.appendChild(divEl);
+     }
+     });
+     
+}
 
 // A function to add the task to localstorage
 function addTaskItem(event) {
      event.preventDefault();
      var taskItem = taskInput.value;
-     var itemId = dayjs().format('YYYYMMDDHHmmss');
+     var itemId = "TAM-"+dayjs().format('YYYYMMDDHHmmss')+"";
      if (taskItem && !isEdit) {
           var divEl = document.createElement("div");
           divEl.classList.add("task-item");
@@ -35,6 +55,7 @@ function addTaskItem(event) {
                               <button type="button" class="deletebtn">Delete</button>
                          </div>`;
           taskList.appendChild(divEl);
+          addTaskToLocalStorage(itemId, taskItem);
      }
      else if (taskItem && isEdit) {
           
@@ -46,12 +67,17 @@ function addTaskItem(event) {
 
 }
 
+function addTaskToLocalStorage(itemKey, itemValue) {
+     localStorage.setItem(itemKey, itemValue);
+}
+
 // A function to edit the task to localstorage
 
 // A function to delete the task from localstorage
 
 // EventListeners
 taskForm.addEventListener("submit", addTaskItem);
+getTaskFromLocalStorage();
 
 
 
